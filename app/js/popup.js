783c2b107802f59fe04124e9dc2ad58f916abf89
Ravi Ranjan <ravi848101mnb@gmail.com>
2024-02-01 21:09:59 +0530

    added event lsitener  for popup

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,21 +1,24 @@
 window.onload = function () {
+  // Get the active tab and send a message when the popup opens
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { message: "popup_open" });
   });
 
-  document.getElementsByClassName("analyze-button")[0].onclick = function () {
+// analyze button
+  const analyzeButton = document.getElementsByClassName("analyze-button")[0];
+  analyzeButton.addEventListener("click", function () {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { message: "analyze_site" });
     });
-  };
+  });
 
-  document.getElementsByClassName("link")[0].onclick = function () {
-    chrome.tabs.create({
-      url: document.getElementsByClassName("link")[0].getAttribute("href"),
-    });
-  };
+  //  event  to the link
+  const linkElement = document.getElementsByClassName("link")[0];
+  linkElement.addEventListener("click", function () {
+    chrome.tabs.create({ url: linkElement.getAttribute("href") });
+  });
 };
-
+//show count
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "update_current_count") {
     document.getElementsByClassName("number")[0].textContent = request.count;
